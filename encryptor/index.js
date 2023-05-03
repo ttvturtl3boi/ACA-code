0225fa0f8c9b9c26ffc70f0d6d5ee7b0289d5307
ACAturtl3 <128638845+ttvturtl3boi@users.noreply.github.com>
2023-05-03 12:59:17 -0700

    Create index.js

---
+++ encryptor/index.js
@@ -0,0 +1,10 @@
+console.log('The encryptor js file loaded');
+
+const letters = 'abcdefghijklmnopqrstuvwxyz';
+const encrypt_key = 'bcdefghigklmnopqrstuvwxyz';
+
+console.log(letters[0]; // a
+            
+for (let i= 0; i < letters.length; i++) {
+    console.log(letters[i]);
+}
