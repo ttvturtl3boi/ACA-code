ca23a55832942788fd1a4f03b9b4528924c5b50f
ACAturtl3 <128638845+ttvturtl3boi@users.noreply.github.com>
2023-05-10 12:15:44 -0700

    Update index.js
    eddited

--- encryptor/index.js
+++ encryptor/index.js
@@ -3,8 +3,12 @@ console.log('The encryptor js file loaded');
 const letters = 'abcdefghijklmnopqrstuvwxyz';
 const encrypt_key = 'bcdefghigklmnopqrstuvwxyz';
 
-console.log(letters[0]; // a
+//console.log(letters[0]; // a
+         
+ for (let i = 0 <  10; i++) {
+             console.log('loop: ' +i);
             
-for (let i= 0; i < letters.length; i++) {
-    console.log(letters[i]);
+          
+             for (let i= 0; i < letters.length; i++) {
+   console.log(letters[i]);
 }
