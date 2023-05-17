5dd4f91d25962a5c0ff214e19154f6786f02b205
ACAturtl3 <128638845+ttvturtl3boi@users.noreply.github.com>
2023-05-17 12:40:14 -0700

    Update index.js

--- encryptor/index.js
+++ encryptor/index.js
@@ -1,40 +1,65 @@
-console.log('The encryptor js file loaded');
+console.log('The encryptor js file loaded'); 
 
-const letters = 'abcdefghijklmnopqrstuvwxyz!?.1';
-const encrypt_key = 'bcdefghigklmnopqrstuvwxyz?!1.';
+const letters = 'abcdefghijklmnopqrstuvwxyz';
+const encrypt_key = 'bcdefghijklmnopqrstuvwxyza';
 
 function encrypt(text) {
   let result = '';
-
-for (let i=0; i < text.length; i++) {
-    let letter = text[i];    
+  
+  for (let i = 0; i < text.length; i++) {
+    let letter = text[i];
     let letterIndex = letters.indexOf(letter);
-         
- if (letterIndex < 0) {
-   result+= letter;     
- }
-         
-else {
-  result += encrypt_key[letterIndex];
-  }
- }
-         
+    
+    if (letterIndex < 0) {
+      result += letter;
+    }// if
+    
+    else {
+      result += encrypt_key[letterIndex]; 
+    }// else
+    
+  }// for
+  
   return result;
+  
 }// encrypt
+
 
 function decrypt(text) {
   let result = '';
-
-for (let i=0; i < text.length; i++) {
-    let letter = text[i];    
+  
+  for (let i = 0; i < text.length; i++) {
+    let letter = text[i];
     let letterIndex = encrypt_key.indexOf(letter);
-         
- if (letterIndex < 0) {
-   result+= letter;     
- }
-         
-else {
-  result += letter[letterIndex];
-  }
- }
-         
+    
+    if (letterIndex < 0) {
+      result += letter;
+    }// if
+    
+    else {
+      result += letters[letterIndex]; 
+    }// else
+    
+  }// for
+  
+  return result;
+  
+}// decrypt
+
+// add event listener to the form
+const encryptForm = document.getElementById('encrypt-form');
+encryptForm.addeventlistener('submit', Function(e) {
+  e.preventDefault();
+
+  // get the value of the content
+  let messageElement = document.getElementById('encrypt');
+  let message = messageElement.value;
+
+// encrypt the content
+let encrypted = encrypt(message);  
+
+  // display encrypted message 
+  let encodedMessageElement = document.getElementById('encoded-message');
+  encodedMessageElement.innerText = encrypted;
+});
+
